2c7abf9cc85f4e7870d8bcdcdd14bd9add6e002e
luvjoey <luvjoey@sina.com>
2018-09-27 18:06:15 +0800

    binary search tree

--- binary_search_tree.py
+++ binary_search_tree.py
@@ -6,9 +6,6 @@ class BinarySearchTree:
     def __getitem__(self, item):
         return Node(self, item)
 
-    def __setitem__(self, key, value):
-        return self._tree.__setitem__(key , value)
-
     def __delitem__(self, key):
         return self._tree.__delitem__(key)
 
@@ -21,8 +18,26 @@ class BinarySearchTree:
     def __repr__(self):
         return self.__str__()
 
-    def getvalue(self, item):
-        return self._tree[item - 1]
+    def insert(self, key):
+        if len(self._tree) == 0:
+            self._tree.append(key)
+            return
+        x = self[0]
+        while True:
+            if key < x.key and x.left:
+                x = x.left
+            elif key > x.key and x.right:
+                x = x.right
+            else:
+                if key < x.key and not x.left:
+                    index = x._left
+                else:
+                    index = x._right
+                length = len(self)
+                if index > length:
+                    self._tree.extend(None for _ in range(length, index + 1))
+                self._tree[index] = key
+                break
 
 
 class Node:
@@ -35,18 +50,29 @@ class Node:
 
     @property
     def key(self):
-        return self.tree._tree[self.index] if self.index <= len(self.tree) else None
+        return self.tree._tree[self.index] if self.index < len(self.tree) else None
 
     @property
     def right(self):
-        return Node(self.tree, self._right) if self._right <= len(self.tree) else None
+        return Node(self.tree, self._right) if self._right < len(self.tree) else None
 
     @property
     def left(self):
-        return Node(self.tree, self._left) if self._left <= len(self.tree) else None
+        return Node(self.tree, self._left) if self._left < len(self.tree) else None
+
+    def __bool__(self):
+        return self.key is not None
 
     def __str__(self):
         return f'<BinarySearchTree.Node index: {self.index}, key: {self.key}>'
 
     def __repr__(self):
         return self.__str__()
+
+
+if __name__ == '__main__':
+    a = BinarySearchTree()
+    a.insert(200)
+    a.insert(1000)
+    a.insert(100)
+    print(a)
